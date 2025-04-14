8cc66b15f719a1de135027c2409ef27fc8b0e06d
Ilias Aliev <ilaliev@2-F-7.42heilbronn.de>
2025-04-14 18:57:50 +0200

    first addition to so_long

--- milestone02/so_long/main.c
+++ milestone02/so_long/main.c
@@ -6,7 +6,47 @@
 /*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 19:51:32 by ilaliev           #+#    #+#             */
-/*   Updated: 2025/03/24 19:51:35 by ilaliev          ###   ########.fr       */
+/*   Updated: 2025/04/14 18:57:12 by ilaliev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+
+/*
+	error_code table:
+	(1) ac != 2
+	(2) av[1] != .ber file
+*/
+
+void	error_message(int error_code)
+{
+	if (error_code == 1)
+		exit();
+	if (error_code == 2)
+		exit()
+	// room for errors
+}
+
+void	validate_input(int ac, char **av)
+{
+	char *last_dot;
+
+	if (ac != 2)
+		error_message(1);
+	last_dot = ft_strrchr(av[1], '.');
+	if (!last_dot || (ft_strncmp(last_dot, ".ber", 4) != 0))
+		error_message(2);
+}
+
+int	main(int ac, char **av)
+{
+	validate_input(ac, av);
+
+	// map valid?
+
+	// init all lists and variables
+
+	// game hook
+
+	// end programm
+}
