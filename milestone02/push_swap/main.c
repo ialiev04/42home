59563427cf098641b2aad2103449e21fbfdc3b3a
Ilias Aliev <iliasmop@gmail.com>
2025-05-06 17:49:19 +0200

    cmall changes

--- milestone02/push_swap/main.c
+++ milestone02/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:32:40 by ilaliev           #+#    #+#             */
-/*   Updated: 2025/04/29 17:07:50 by ilaliev          ###   ########.fr       */
+/*   Updated: 2025/04/30 18:23:32 by ilaliev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,17 @@ int	main(int ac, char **av)
 	if (ac < 2 || (ac == 2 && !*av[1]))
 		return (error_code(0));
 	if (ac == 2)
-		split(av[1]); //todo
+		av = split(av[1], ' '); //todo
+	stack_a(&a, av + 1);	//todo
+	if (!stack_sorted(a))	//todo
+	{
+		if (stack_len(a) == 2)	//todo
+			sa(&a);	//todo
+		else if (stack_len(a) == 3)
+			sort_three(&a);	//todo
+		else
+			sort_stack(&a, &b);	//todo
+	}
+	free_all();	//todo
+	return (1);
 }
---
+++ milestone02/push_swap/split.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 18:25:48 by ilaliev           #+#    #+#             */
+/*   Updated: 2025/04/30 18:30:50 by ilaliev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	ft_wordct(char *s, char c)
+{
+	int	ct;
+	int	in_word;
+
+	ct = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			in_word = 1;
+			ct++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (ct);
+}
+
+static int	ft_word_len(char *s, char c, int j)
+{
+	int	i;
+
+	i = 0;
+	while (s[j + i] != c && s[j + i] != '\0')
+		i++;
+	return (i);
+}
+
+static char	*ft_fillword(char *s, char c, int j)
+{
+	char	*ret;
+	int		i;
+	int		word_len;
+
+	i = 0;
+	word_len = ft_word_len(s, c, j);
+	ret = ft_calloc(word_len + 1, sizeof(char));
+	if (ret == NULL)
+		return (NULL);
+	while (i < word_len)
+		ret[i++] = s[j++];
+	ret[i] = '\0';
+	return (ret);
+}
+
+static void	*ft_free_split(char **ret, unsigned int how_many)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < how_many)
+	{
+		free(ret[i]);
+		i++;
+	}
+	free(ret);
+	return (NULL);
+}
+
+char	**ft_split(char *s, char c)
+{
+	char			**ret;
+	unsigned int	words;
+	unsigned int	i;
+	unsigned int	j;
+
+	words = ft_wordct(s, c);
+	if (words < 2)		// not sure lol
+		error_code(2)
+	ret = ft_calloc(words + 1, sizeof(char *));
+	if (ret == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < words)
+	{
+		while (s[j] == c)
+			j++;
+		ret[i] = ft_fillword((char *)s, c, j);
+		if (ret[i] == NULL)
+			return (ft_free_split(ret, i));
+		j = j + ft_word_len((char *)s, c, j);
+		i++;
+	}
+	ret[i] = NULL;
+	return (ret);
+}
