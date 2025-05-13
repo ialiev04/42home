f84d2767496aa40445674457e8217f0ab63e606c
Ilias Aliev <ilaliev@2-F-3.42heilbronn.de>
2025-05-13 15:59:21 +0200

    added libft

---
+++ milestone02/push_swap/libft/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 21:50:04 by ilaliev           #+#    #+#             */
+/*   Updated: 2025/03/11 22:06:48 by ilaliev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ret;
+	size_t	i;
+
+	i = ft_strlen(s1);
+	ret = malloc(i + 1);
+	if (ret == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ret[i] = s1[i];
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
