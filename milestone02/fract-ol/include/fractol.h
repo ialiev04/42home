55b2fd67d466b9517bfd472790705b3c98d89b23
Ilias Aliev <ilaliev@1-D-18.42heilbronn.de>
2025-06-27 18:58:00 +0200

    started on my fractol code

---
+++ milestone02/fract-ol/include/fractol.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 16:25:37 by ilaliev           #+#    #+#             */
+/*   Updated: 2025/06/27 18:46:08 by ilaliev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+# define WIDTH 512
+# define HEIGHT 512
+
+# define COLOR_WHITE	0xFFFFFFFF
+# define COLOR_BLACK	0x000000FF
+# define COLOR_RED		0xFF0000FF
+# define COLOR_GREEN	0x00FF00FF
+# define COLOR_BLUE		0x0000FFFF
+# define COLOR_YELLOW	0xFFFF00FF
+# define COLOR_CYAN		0x00FFFFFF
+# define COLOR_MAGENTA	0xFF00FFFF
+# define COLOR_GRAY		0x808080FF
+# define COLOR_ORANGE	0xFFA500FF
+# define COLOR_PINK		0xFFC0CBFF
+# define COLOR_PURPLE	0x800080FF
+# define COLOR_BROWN	0xA52A2AFF
+# define COLOR_LIME		0xBFFF00FF
+# define COLOR_SKYBLUE	0x87CEEBFF
+
+#endif
--- milestone02/fract-ol/main.c
+++ milestone02/fract-ol/main.c
@@ -6,90 +6,31 @@
 /*   By: ilaliev <ilaliev@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 16:38:47 by ilaliev           #+#    #+#             */
-/*   Updated: 2025/06/27 14:53:09 by ilaliev          ###   ########.fr       */
+/*   Updated: 2025/06/27 18:54:58 by ilaliev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "MLX42/include/MLX42/MLX42.h"
+#include "include/fractol.h"
 
-#define WIDTH 512
-#define HEIGHT 512
-
-static mlx_image_t* image;
-
-// -----------------------------------------------------------------------------
-
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		mlx_close_window(param);
 }
 
-void ft_randomize(void* param)
+int	main(void)
 {
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
-	{
-		for (uint32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			mlx_put_pixel(image, i, y, color);
-		}
-	}
-}
+	mlx_t		*mlx;
+	mlx_image_t	*img;
 
-void ft_hook(void* param)
-{
-	mlx_t* mlx = param;
-
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-}
-
-// -----------------------------------------------------------------------------
-
-int32_t main(void)
-{
-	mlx_t* mlx;
-
-	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
-
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
+		return (1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img)
+		return (1);
+	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_key_hook(mlx, my_keyhook, mlx);
 	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+	return (0);
 }
